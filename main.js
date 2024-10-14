bc9867d461692f48c6595e3aef3faba44643bb1d
JMReich <149850274+JMReich@users.noreply.github.com>
2024-10-14 16:57:55 -0400

    Skeleton functions for drive mounting/displaying
    Added some skeleton functions for mounting/displaying network drives. Also added a minimum width and height to the electron app.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function createWindow () {
     const win = new BrowserWindow({
         width: 1920,
         height: 1080,
+        minWidth: 800,
+        minHeight: 600,
         webPreferences: {
         nodeIntegration: true
         }
